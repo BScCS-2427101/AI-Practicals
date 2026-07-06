550476b422c9b5022c72c655af8d38aaaa045a27
T101(BSc.CS-2027) <parabrajdeepcs242555@gmail.com>
2026-07-06 22:24:09 +0530

    Update Code_B.py
    Updated the graph structure and modified the iterative DFS function to include new nodes and paths. Enhanced output formatting for the results.

--- AI_Practical-1/Code_B.py
+++ AI_Practical-1/Code_B.py
@@ -1,12 +1,16 @@
 import time
 graph = {
-    "Home": [("Bus Stop", 2), ("Hospital", 3), ("Park", 4)],
+    "Home": [("Bus Stop", 2), ("Hospital", 3), ("Park", 2)],
     "Bus Stop": [("Railway", 2)],
+    "Railway": [("Library", 1)],
+    "Library": [("College", 2)],
     "Hospital": [("Temple", 2)],
+    "Temple": [("Garden", 2)],
+    "Garden": [("College", 2)],
     "Park": [("Mall", 2)],
-    "Railway": [("College", 3)],
-    "Temple": [("College", 2)],
-    "Mall": [("College", 4)],
+    "Mall": [("Cinema", 2)],
+    "Cinema": [("Stadium", 2)],
+    "Stadium": [("College", 3)],
     "College": []
 }
 def iterative_dfs(start, goal):
@@ -20,13 +24,13 @@ def iterative_dfs(start, goal):
             return path, distance, (end_time - start_time) * 1000
         if node not in visited:
             visited.add(node)
-            for neighbour, dist in reversed(graph[node]):
+            for neighbour, dist in graph[node]:
                 stack.append((neighbour,
                               path + [neighbour],
                               distance + dist))
     return None
 path, distance, t = iterative_dfs("Home", "College")
-print("Iterative DFS Result")
+print("===== Iterative DFS Result =====")
 print("Path :", " -> ".join(path))
 print("Distance :", distance, "km")
 print("Time :", round(t, 5), "ms")
