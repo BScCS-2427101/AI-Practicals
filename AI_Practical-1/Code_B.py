4c377af12beb419766b7fa7cd04a916ab28b3a73
T101(BSc.CS-2027) <parabrajdeepcs242555@gmail.com>
2026-07-06 21:01:55 +0530

    Update Code_B.py
    Replaced the previous DFS implementation with an iterative DFS that calculates the path and distance between nodes in a weighted graph.

--- AI_Practical-1/Code_B.py
+++ AI_Practical-1/Code_B.py
@@ -1,28 +1,32 @@
+import time
 graph = {
-    'A': ['B', 'C'],
-    'B': ['D', 'E'],
-    'C': ['F', 'G'],
-    'D': [],
-    'E': [],
-    'F': [],
-    'G': []
+    "Home": [("Bus Stop", 2), ("Hospital", 3), ("Park", 4)],
+    "Bus Stop": [("Railway", 2)],
+    "Hospital": [("Temple", 2)],
+    "Park": [("Mall", 2)],
+    "Railway": [("College", 3)],
+    "Temple": [("College", 2)],
+    "Mall": [("College", 4)],
+    "College": []
 }
-
-visited = []
-stack = []
-
-start = 'A'
-
-stack.append(start)
-
-print("DFS Traversal:")
-
-while stack:
-    node = stack.pop()
-
-    if node not in visited:
-        print(node, end=" ")
-        visited.append(node)
-
-        for neighbour in reversed(graph[node]):
-            stack.append(neighbour)
+def iterative_dfs(start, goal):
+    stack = [(start, [start], 0)]
+    visited = set()
+    start_time = time.perf_counter()
+    while stack:
+        node, path, distance = stack.pop()
+        if node == goal:
+            end_time = time.perf_counter()
+            return path, distance, (end_time - start_time) * 1000
+        if node not in visited:
+            visited.add(node)
+            for neighbour, dist in reversed(graph[node]):
+                stack.append((neighbour,
+                              path + [neighbour],
+                              distance + dist))
+    return None
+path, distance, t = iterative_dfs("Home", "College")
+print("Iterative DFS Result")
+print("Path :", " -> ".join(path))
+print("Distance :", distance, "km")
+print("Time :", round(t, 5), "ms")
